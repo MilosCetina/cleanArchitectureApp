4e784b53d5ab354f1aefdd3a5abaabb6e2c99ce3
Milos Cetina <miloscetina4@gmail.com>
2022-09-06 09:05:33 +0200

    added data model and tests for that and json files for testig data from API

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:hello_world/core/error/failures.dart';
 import 'package:hello_world/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:hello_world/features/number_trivia/domain/repositories/number_trivia_repository.dart';
--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,5 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:hello_world/features/number_trivia/domain/entities/number_trivia.dart';
 
-void main(){
-  //final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
+void main() {
+  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
+
+  test(
+    'should be a subclass of NumberTrivia entity',
+    () async {
+      //assert
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
+    },
+  );
 }
